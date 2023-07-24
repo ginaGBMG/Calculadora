0fa4ca9b5a696b0fb396552290e1650576419e7a
Gina B Molina G <137374097+ginaGBMG@users.noreply.github.com>
2023-07-24 08:59:09 -0600

    Se añade la calculadora oficial

---
+++ js/main.js
@@ -0,0 +1,19 @@
+/*const displayValorAnterior = document.getElementById('valor-anterior');
+const displayValorActual = document.getElementById('valor-actual');
+const botonesNumeros = document.querySelectorAll('numero');
+const botonesOperadores = document.querySelectorAll('operadores');
+
+/*const calculadora = new Calculadora();
+console.log(calculadora.sumar(2,3))
+console.log(calculadora.restarr(2,3))
+console.log(calculadora.multiplicar(2,3))
+console.log(calculadora.dividir(2,3)) */
+
+/*const display = new Display(displayValorAnterior, displayValorActual);
+
+botonesNumeros.forEach(boton => {
+    boton.addEventListener('click',() => display.agregarNumero (boton.innerHTML));
+});
+botonesOperadores.forEach(boton => {
+        boton.addEventListener('click',() => display.agregarOperadores (boton.innerHTML));
+});/*
